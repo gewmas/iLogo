c2583e429dad0cde9271575727bc135d4a25aeeb
Yuhua Mai <yuhua.mai@duke.edu>
2013-10-25 00:47:56 -0400

    draw lines works

---
+++ iLogo/iLogo/draw2D.h
@@ -0,0 +1,13 @@
+//
+//  draw2D.h
+//  iLogo
+//
+//  Created by Yuhua Mai on 10/8/13.
+//  Copyright (c) 2013 Yuhua Mai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface draw2D : UIView
+
+@end
